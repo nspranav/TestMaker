8a1df9ae2712b0df26bf33d56179fb593ccd499e
Pranav Ns <nspranav1180@gmail.com>
2018-08-07 07:21:29 -0400

    added less to quiz-list component

--- ClientApp/app/components/quiz/quiz-search.component.ts
+++ ClientApp/app/components/quiz/quiz-search.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ViewEncapsulation } from "@angular/core";
 
 @Component({
     selector: "quiz-search",
     templateUrl: "quiz-search.component.html",
-    styleUrls: ["./quiz-search.component.css"]
+    styleUrls: ["./quiz-search.component.css"],
+    encapsulation: ViewEncapsulation.None
 })
 export class QuizSearchComponent {
     @Input() class:string;
